e9cf6d946cea45aa4c2b4682369e7d68acb47633
Jonathan <atanajonathan5@gmail.com>
2025-05-30 21:49:56 +0100

    Update project structure: forgot some files in previous commit (chap2/ALB-ASG)

---
+++ chap2/stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,18 @@
+# Define input variables
+variable "server_http_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "alb_http_port" {
+  description = "The port the ALB will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+variable "filter_names" {
+  description = "List of names to use for filtering AMIs in the data source block"
+  type        = list(string)
+  default     = ["amzn2-ami-hvm-*-x86_64-gp2"] # default filter name for Amazon Linux 2 AMI
+}
