cca9a26a1035e88b9b8b1c130c2e0b8df2d23ab2
Jonathan <atanajonathan5@gmail.com>
2025-06-10 22:25:52 +0100

    Move modules to my github modules repo

--- chap2/modules/storage/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db-address" {
-  description = "Connect to the MySQL database at this endpoint"
-  value       = aws_db_instance.main.address
-}
-
-output "db-port" {
-  description = "The port on which the MySQL database is listening/accessible"
-  value       = aws_db_instance.main.port
-}
